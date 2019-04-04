0229353fcaa55cc8c8091e166a6730eb62390890
Manuel Alvarez <manuel@pythiac.com>
2019-04-04 15:29:35 +0200

    Add fit_score_model utility

--- tgan/research.py
+++ tgan/research.py
@@ -1,12 +1,14 @@
 """Tune and evaluate TGAN models."""
+import os
 
 import numpy as np
+import pandas as pd
+import tensorflow as tf
 from sklearn.model_selection import train_test_split
+from tensorpack.utils import logger
 
-from tgan.data import load_data
+from tgan.evaluation import evaluate_classification
 from tgan.model import TUNABLE_VARIABLES, TGANModel
-
-# from tgan.evaluation import evaluate_classification
 
 
 def prepare_hyperparameter_search(steps_per_epoch, num_random_search):
@@ -15,8 +17,8 @@ def prepare_hyperparameter_search(steps_per_epoch, num_random_search):
     basic_kwargs = {
         'max_epoch': 1,
         'steps_per_epoch': steps_per_epoch,
-        'store_checkpoints': True
     }
+
     for i in range(num_random_search):
         kwargs = {name: np.random.choice(choices) for name, choices in TUNABLE_VARIABLES.items()}
         kwargs.update(basic_kwargs)
@@ -25,9 +27,35 @@ def prepare_hyperparameter_search(steps_per_epoch, num_random_search):
     return model_kwargs
 
 
+def fit_score_model(
+        name, model_kwargs, train_data, test_data, continuous_columns,
+        sample_rows, store_samples):
+
+    for index, kwargs in enumerate(model_kwargs):
+        logger.info('Training TGAN Model %d/%d', index + 1, len(model_kwargs))
+
+        tf.reset_default_graph()
+        model = TGANModel(continuous_columns, output='{}/model_{}'.format(name, index), **kwargs)
+        model.fit(train_data)
+        sampled_data = model.sample(sample_rows)
+
+        if store_samples:
+            dir_name = '{}/data'.format(name)
+            if not os.path.isdir(dir_name):
+                os.mkdir(dir_name)
+
+            file_name = os.path.join(dir_name, 'model_{}.csv'.format(index))
+            sampled_data.to_csv(file_name, index=False, header=True)
+
+        score = evaluate_classification(sampled_data, test_data, continuous_columns)
+        model_kwargs[index]['score'] = score
+
+    return model_kwargs
+
+
 def run_experiment(
     name, max_epoch, steps_per_epoch, output_epoch, sample_rows, file_path, continuous_columns,
-    num_random_search
+    num_random_search, store_samples=True, force=False
 ):
     """Run experiment using the given params and collect the results.
 
@@ -40,18 +68,13 @@ def run_experiment(
 
     """
     # Load and split data
-    data = load_data(file_path)
-    test_index, train_index = train_test_split(data, train_size=0.8)
+    data = pd.read_csv(file_path, header=-1)
+    train_data, test_data = train_test_split(data, train_size=0.8)
 
     # Prepare hyperparameter search
     model_kwargs = prepare_hyperparameter_search(steps_per_epoch, num_random_search)
 
-    synthesized_data = {}
-
-    # Training models and sampling data
-    for index, kwargs in enumerate(model_kwargs):
-        for epoch in range(max_epoch):
-            model = TGANModel(**kwargs, output='{}}/model_{}'.format(name, index))
-            model.fit(data)
-            synthesized_data[(index, epoch)] = model.sample(sample_rows)
-    # Evaluating synthesized data
+    return fit_score_model(
+        name, model_kwargs, train_data, test_data,
+        continuous_columns, sample_rows, store_samples
+    )
